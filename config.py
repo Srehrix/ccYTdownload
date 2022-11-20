2cd3ac2349bf09c79345a93bde77583c2e1a47da
Sree Hari <98310399+Srehrix@users.noreply.github.com>
2022-11-20 14:51:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import  os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 APP_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
-
+SPIC = os.environ.get("START_PIC")
 youtube_next_fetch = 0  # time in minute
 
 
